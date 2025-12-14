42f3407237d68c1aa301fef1920eec2dac9056e7
Chamseddine-svg <khlifichamseddine@gmail.com>
2025-12-14 13:05:15 +0100

    do to ex6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,times) {
+    let finalString=""
+    if (times <= 0){
+        return "Enter positive number grater than 0"
+    }else{
+        for (let i=1;i<=times;i++){
+            finalString += string
+        }
+    return finalString
+    }
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split("")
+    let reversedArr = []
+    let len = arr.length
+    for(let i = 0 ; i <= arr.length-1 ; i++){
+        reversedArr[i] = arr[arr.length-1-i]
+        reversedArr.join()
+    }
+return reversedArr
 };
 
 // Do not edit below this line
